38bcc54176f7c188bd7e0326ebc63c53c279993b
Jean Lalande <jeanlalande@gmail.com>
2017-08-12 13:10:20 -0400

    Add src tag update in the directive's componentUpdated method

--- vue-auth-image.js
+++ vue-auth-image.js
@@ -8,20 +8,31 @@
     throw new Error('[vue-auth-image] cannot locate Axios');
   }
 
+  function setImgSrc(el, binding) {
+    if (binding.oldValue === undefined || binding.value !== binding.oldValue) {
+      var imageUrl = binding.value;
+      axios({
+        method: 'get',
+        url: imageUrl,
+        responseType: 'arraybuffer'
+      })
+      .then(function(resp) {
+        var mimeType = resp.headers['content-type'].toLowerCase();
+        var imgBase64 = new Buffer(resp.data, 'binary').toString('base64');
+        el.src = 'data:' + mimeType + ';base64,' + imgBase64;
+      }).catch((function() {
+        el.src = imageUrl;
+      }));
+    }
+  }
+
   vueAuthImage.install = function(Vue) {
     Vue.directive('auth-image', {
       bind: function(el, binding) {
-        var imageUrl = binding.value;
-        axios({
-          method: 'get',
-          url: imageUrl,
-          responseType: 'arraybuffer'
-        })
-        .then(function(resp) {
-          var mimeType = resp.headers['content-type'].toLowerCase();
-          var imgBase64 = new Buffer(resp.data, 'binary').toString('base64');
-          el.src = 'data:' + mimeType + ';base64,' + imgBase64;
-        });
+        setImgSrc(el, binding);
+      },
+      componentUpdated: function(el, binding) {
+        setImgSrc(el, binding);
       }
     });
   };
